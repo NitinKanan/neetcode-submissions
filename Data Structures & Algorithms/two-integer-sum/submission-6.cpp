00f02b933f31f5e628f50e4bb781824182540418
NitinKanan <nitin.kanan2001@gmail.com>
2026-04-23 22:45:45 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // // sorting approach
+        // // define a vector of pair to store the nums
+        // vector<pair<int, int>> A;
+        // // store nums
+        // for (int i = 0; i < nums.size(); i++) A.push_back({nums[i], i});
+        // // sort the vector -> takes O(n log n)
+        // sort(A.begin(), A.end());
+        // // apply two pointer approach
+        // int i = 0;
+        // int j = nums.size() - 1;
+        // while (i < j) {
+        //     if (A[i].first + A[j].first == target) return {min(A[i].second, A[j].second), max(A[i].second, A[j].second)};
+        //     else if (A[i].first + A[j].first < target) i++;
+        //     else j--;
+        // }
+        // return {};
+
+        // solution using hashing
+        unordered_map<int, int> A;
+        for (int i = 0; i < nums.size(); i++) {
+            A[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if(A.count(diff) && A[diff] != i) return {min(i, A[diff]), max(i, A[diff])};
+        }
+        return {};
+    }
+};
