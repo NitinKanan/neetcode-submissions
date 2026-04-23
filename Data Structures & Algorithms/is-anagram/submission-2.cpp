9fafe07d555ea0770e32406f61c52831089f128c
NitinKanan <nitin.kanan2001@gmail.com>
2026-04-23 20:20:26 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // check if the lengths are unequal then it must not be a anagram
+        if (s.length() != t.length()) return false;
+        // sorting solution
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        
+        // return s == t;
+        
+        // hashing solution
+        unordered_map<char, int> count;
+        for (char c : s) {
+            count[c]++;
+        }
+        for (char c : t) {
+            count[c]--;
+            if (count[c] < 0) return false;
+        }
+        return true;
+    }
+};
